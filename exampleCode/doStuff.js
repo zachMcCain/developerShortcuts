e95ef5e6dbbe8d46cb3ec4779c623b23ba3e9ba5
Zachary McCain <zacmmcca@gmail.com>
2021-01-21 18:17:56 -0600

    Putting the fun in function.

---
+++ exampleCode/doStuff.js
@@ -0,0 +1,14 @@
+
+
+function cloneSheps(cohort) {
+  for (var i = 0; i < cohort.length; i++) {
+    if (i % 2 === 0) {
+      cohort[i] = 'Tre Moore';
+    } else {
+      cohort[i] = 'Zach McCain';
+    }
+  }
+  return cohort;
+}
+
+module.exports = cloneSheps;
---
+++ index.js
@@ -0,0 +1,7 @@
+const clone = require('./exampleCode/doStuff.js');
+
+
+
+let newClass = clone(['Harry Potter', 'Salamander Slytherin', 'Joe Exotic', 'Mando'])
+
+console.log(newClass);
