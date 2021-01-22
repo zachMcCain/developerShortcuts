821f715468ad8b888a5b439a9671c31067bc654c
Zachary McCain <zacmmcca@gmail.com>
2021-01-22 13:13:01 -0600

    Updated developerShortcuts code.

--- exampleCode/keepStuff.js
+++ exampleCode/keepStuff.js
@@ -1,21 +1,69 @@
+const weirdlyNestedExample = {
+  nestedStuff: {
+    aNestedArray: [
+      1,
+      2,
+      'buckle my shoe',
+      3,
+      4,
+      'shut the door'
+    ],
+    aNestedObject: {
+      aMoreDeeplyNestedArray: [
+        true,
+        {
+          someStuff: [
+            'it\'s kindof dark in here....',
+            false,
+            true,
+            1234,
+            'pizza is good'
+          ]
+        },
+        45,
+        234,
+        9019,
+        false
+      ]
+    },
+    info: 'I\'m at the end.'
+  },
+  lastNote: 'No, I am.'
+}
+
+
+
+
 const sheps = ['Tre Moore', 'Zach McCain'];
-const cohort = ['Harry Potter', 'Salamander Slytherin', 'Joe Exotic', 'Mando'];
+
+
+const rfe1 = [
+  'Colleen Handel',
+  'Aaron Faulkner',
+  'Johan Bejar',
+  'William Mehlenbacher',
+  'Adrian Rodriguez',
+  'aubrey bachant',
+  'Stephen Hyde',
+  'kaleb lyda',
+  'Bob Kelly',
+  'Carmen Hilbert',
+  'Jodi Silverman',
+  'Joseph Haller',
+  'Zach Kodjo',
+  'Sung Kim',
+  'Sara Landis',
+  'Squall Leonhart',
+  'Phillip Teves',
+  'Erik Dayton',
+  'Ross Loney',
+  'Cody Gonzalez',
+  'Daniel Doyle',
+  'Jake Nalls',
+  'Jihang Li'
+];
 
 module.exports = {
   sheps: sheps,
-  cohort: cohort
+  cohort: rfe1
 }
-
-// Pairs:
-//     Colleen Handel & Aaron Faulkner
-//     Johan Bejar & William Mehlenbacher
-//     Adrian Rodriguez & aubrey bachant
-//     Stephen Hyde & kaleb lyda
-//     Bob Kelly & Carmen Hilbert
-//     Jodi Silverman & Joseph Haller
-//     Zach Kodjo & Sung Kim
-//     Sara Landis & Squall Leonhart
-//     Phillip Teves & Erik Dayton
-//     Ross Loney & Cody Gonzalez
-//     Daniel Doyle & Jake Nalls
-//     Jihang Li
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const clone = require('./exampleCode/doStuff.js');
 const stuff = require('./exampleCode/keepStuff.js');
+const cohort = stuff.cohort;
+const sheps = stuff.sheps;
 
-
-
-let newClass = clone(stuff.cohort, stuff.sheps)
+let newClass = clone(cohort, sheps);
 
 console.log(newClass);
